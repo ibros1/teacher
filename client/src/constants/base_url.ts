60b26eae5d0b30b2396549cf25c8a3848988e322
ibros1 <maxamedsuhayb41@gmail.com>
2026-03-22 20:46:03 +0300

    added bse url system

--- client/src/constants/base_url.ts
+++ client/src/constants/base_url.ts
@@ -1,3 +1,2 @@
-export const BASE_API_URL = "http://localhost:5000";
-// http://localhost:5000/
-// https://e-learning-api-full.vercel.app
+// export const BASE_API_URL = "http://localhost:5000";
+export const BASE_API_URL = "https://teacher-3tpd.vercel.app";
